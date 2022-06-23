b1a656b6a3385aed427b17cf39a869302e7f51a9
Rafael Oliveira <raf100guy@gmail.com>
2022-06-23 19:42:51 -0300

    initial commit

---
+++ reviews-api/src/main/java/com/company/reviewsapi/ReviewsApiApplication.java
@@ -0,0 +1,13 @@
+package com.company.reviewsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewsApiApplication.class, args);
+    }
+
+}
